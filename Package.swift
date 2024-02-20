a89724ee26dbd6ec95a738da790488500efaa3b8
xcode73 <xcode.uln@gmail.com>
2024-02-20 14:13:48 +0400

    update vapor to 4.92.3

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Feather", targets: ["Feather"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor", from: "4.70.0"),
+        .package(url: "https://github.com/vapor/vapor", from: "4.92.3"),
         .package(url: "https://github.com/vapor/fluent", from: "4.4.0"),
         .package(url: "https://github.com/binarybirds/liquid", from: "1.3.0"),
         .package(url: "https://github.com/binarybirds/mail", from: "0.0.1"),
