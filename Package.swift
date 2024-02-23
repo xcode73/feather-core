6729ce939373604af0ac12ca86c4fe8434eb9c72
xcode73 <xcode.uln@gmail.com>
2024-02-23 22:00:46 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
         .library(name: "Feather", targets: ["Feather"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor", from: "4.92.3"),
-        .package(url: "https://github.com/vapor/fluent", from: "4.9.0"),
-        .package(url: "https://github.com/binarybirds/liquid", from: "1.3.2"),
-        .package(url: "https://github.com/binarybirds/mail", from: "0.0.2"),
-        .package(url: "https://github.com/binarybirds/swift-html", from: "1.7.0"),
-        .package(url: "https://github.com/xcode73/feather-objects", branch: "test-dev"),
-        .package(url: "https://github.com/xcode73/feather-icons", branch: "test-dev"),
-        .package(url: "https://github.com/binarybirds/spec", from: "1.2.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.92.3"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        .package(url: "https://github.com/binarybirds/liquid.git", from: "1.3.2"),
+        .package(url: "https://github.com/binarybirds/mail.git", from: "0.0.2"),
+        .package(url: "https://github.com/binarybirds/swift-html.git", from: "1.7.0"),
+        .package(url: "https://github.com/xcode73/feather-objects.git", branch: "test-dev"),
+        .package(url: "https://github.com/xcode73/feather-icons.git", branch: "test-dev"),
+        .package(url: "https://github.com/binarybirds/spec.git", from: "1.2.0")
     ],
     targets: [
         .target(name: "Feather", dependencies: [
@@ -49,4 +49,3 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v5]
 )
-
